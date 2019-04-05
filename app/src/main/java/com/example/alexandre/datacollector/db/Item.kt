5d3b91c4592a6f3d9f93cd79c96845d76d49b3fd
Alexandre Ohara <alexandre.ohara@hotmail.com>
2019-04-05 20:47:58 -0300

    correcao nas classes

--- app/src/main/java/com/example/alexandre/datacollector/db/Item.kt
+++ app/src/main/java/com/example/alexandre/datacollector/db/Item.kt
@@ -1,5 +1,27 @@
 package com.example.alexandre.datacollector.db
 
-/**
- * Created by alexandre on 04/04/19.
- */
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+@Entity(tableName = "item_table")
+
+data class Item(
+        var name: String,
+        var deploymentState: String,
+        var incidentState: String,
+        var vendor: String,
+        var model: String,
+        var description: String,
+        var type: String,
+        var owner: String,
+        var serialNumber: String,
+        var operatingSystem: String,
+        var graphicAdapter: String,
+        var otherEquipment: String,
+        var warrantyExpirationDate: String,
+        var installDate: String,
+        var note: String
+) {
+    @PrimaryKey(autoGenerate = false)
+    var number: Int = 0
+}
--- app/src/main/java/com/example/alexandre/datacollector/db/ItemDatabase.kt
+++ app/src/main/java/com/example/alexandre/datacollector/db/ItemDatabase.kt
@@ -1,27 +1,2 @@
 package com.example.alexandre.datacollector.db
 
-import android.arch.persistence.room.Entity
-import android.arch.persistence.room.PrimaryKey
-
-@Entity(tableName = "item_table")
-
-data class Item(
-        var name: String,
-        var deploymentState: String,
-        var incidentState: String,
-        var vendor: String,
-        var model: String,
-        var description: String,
-        var type: String,
-        var owner: String,
-        var serialNumber: String,
-        var operatingSystem: String,
-        var graphicAdapter: String,
-        var otherEquipment: String,
-        var warrantyExpirationDate: String,
-        var installDate: String,
-        var note: String
-) {
-    @PrimaryKey(autoGenerate = false)
-    var number: Int = 0
-}
