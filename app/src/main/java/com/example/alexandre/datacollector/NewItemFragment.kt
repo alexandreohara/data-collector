f7e4aacb0e4254ba726054d32a09228e45002436
Alexandre Ohara <alexandre.ohara@hotmail.com>
2019-04-07 17:16:00 -0300

    mudancas no db e ligacao do ViewModel com a tela

--- app/src/main/java/com/example/alexandre/datacollector/NewItemFragment.kt
+++ app/src/main/java/com/example/alexandre/datacollector/NewItemFragment.kt
@@ -1,15 +1,18 @@
 package com.example.alexandre.datacollector
 
 
+import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.navigation.findNavController
+import com.example.alexandre.datacollector.item.ItemViewModel
+import com.example.alexandre.datacollector.item.ItemViewModelFactory
 import com.example.alexandre.datacollector.databinding.AddNewItemBinding
+import com.example.alexandre.datacollector.db.ItemDatabase
 
 /**
  * A simple [Fragment] subclass.
@@ -19,10 +22,20 @@ class NewItemFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val binding = DataBindingUtil.inflate<AddNewItemBinding>(inflater, R.layout.add_new_item, container, false)
+        val binding: AddNewItemBinding = DataBindingUtil.inflate(inflater, R.layout.add_new_item, container, false)
         binding.t1ContinueButton.setOnClickListener { v ->
             v.findNavController().navigate(R.id.action_newItemFragment_to_detailsFragment2)
         }
+
+        // referencia do application que este fragmento está ligado para passar pro ViewModelProvider
+        val application = requireNotNull(this.activity).application
+        val dataSource = ItemDatabase.getInstance(application).itemDao()
+        val viewModelFactory = ItemViewModelFactory(dataSource, application)
+        val itemViewModel = ViewModelProviders.of(this, viewModelFactory).get(ItemViewModel::class.java)
+
+        binding.itemViewModel = itemViewModel
+        binding.setLifecycleOwner(this)
+
         return binding.root
     }
 
--- app/src/main/java/com/example/alexandre/datacollector/item/ItemViewModel.kt
+++ app/src/main/java/com/example/alexandre/datacollector/item/ItemViewModel.kt
@@ -1,12 +1,14 @@
-package com.example.alexandre.datacollector.Item
+package com.example.alexandre.datacollector.item
 
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
+import com.example.alexandre.datacollector.db.ItemDao
 
 /**
  * Created by alexandre on 04/04/19.
  */
 
-class ItemViewModel(application: Application) : AndroidViewModel(application) {
-    private var repository: ItemRepository = ItemRepository(application)
+class ItemViewModel(
+        val database: ItemDao,
+        application: Application) : AndroidViewModel(application) {
 }
