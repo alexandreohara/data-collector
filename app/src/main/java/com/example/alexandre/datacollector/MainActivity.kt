de01411b47d0786e94a2338adf8e38e1d91464f9
Alexandre Ohara <alexandre.ohara@hotmail.com>
2019-04-15 19:41:22 -0300

    transicao do banco de dados de activity para fragment

--- app/src/main/java/com/example/alexandre/datacollector/MainActivity.kt
+++ app/src/main/java/com/example/alexandre/datacollector/MainActivity.kt
@@ -27,85 +27,85 @@ import java.util.*
 import kotlin.coroutines.CoroutineContext
 
 
-class MainActivity : AppCompatActivity(), CoroutineScope {
+class MainActivity : AppCompatActivity() {//, CoroutineScope {
 
     private val paths = arrayOf("item 1", "item 2", "item 3","item 1", "item 2", "item 3","item 1", "item 2", "item 3","item 1", "item 2", "item 3","item 1", "item 2", "item 3","item 1", "item 2", "item 3","item 1", "item 2", "item 3","item 1", "item 2", "item 3","item 1", "item 2", "item 3","item 1", "item 2", "item 3","item 1", "item 2", "item 3","item 1", "item 2", "item 3","item 1", "item 2", "item 3")
     private lateinit var binding: ActivityMainBinding
-    private lateinit var job: Job
-    override val coroutineContext: CoroutineContext
-        get() = job + Dispatchers.Main
+//    private lateinit var job: Job
+//    override val coroutineContext: CoroutineContext
+//        get() = job + Dispatchers.Main
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        val application = requireNotNull(this).application
-        job = Job()
-        launch {
-            async(Dispatchers.Default) {
-                readCSV(application)
-            }
-        }
+//        val application = requireNotNull(this).application
+//        job = Job()
+//        launch {
+//            async(Dispatchers.Default) {
+//                readCSV(application)
+//            }
+//        }
 
 
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        job.cancel()
-    }
+//    override fun onDestroy() {
+//        super.onDestroy()
+//        job.cancel()
+//    }
 
 
-    private fun readCSV(application: Application) {
-
-        var fileReader: BufferedReader? = null
-        var csvReader: CSVReader? = null
-        try {
-            fileReader = applicationContext.assets.open("Export_Ramal.csv").bufferedReader()
-            csvReader = CSVReaderBuilder(fileReader).
-                    withCSVParser(CSVParserBuilder().
-                            withSeparator(';').
-                            withIgnoreQuotations(false).
-                            withFieldAsNull(CSVReaderNullFieldIndicator.BOTH).
-                            build()).
-                    withSkipLines(1).build()
-            var line: Array<String>?
-            line = csvReader.readNext()
-            while (line != null) {
-                var item = Item(
-                        number = line[0],
-                        name = line[1],
-                        deploymentState = line[2],
-                        incidentState = line[3],
-                        vendor = line[4],
-                        model = line[5],
-                        description = line[6],
-                        type = line[7],
-                        owner = line[8],
-                        serialNumber = line[9]
-                )
-                val dataSource = ItemDatabase.getInstance(application).itemDao()
-                println(dataSource.getRowsCount())
-                //println(item)
-                dataSource.insert(item)
-                //PopulateDbAsync(item, application)
-                line = csvReader.readNext()
-            }
-            csvReader.close()
-        } catch (e: Exception) {
-            println("Reading CSV Error!")
-            e.printStackTrace()
-        } finally {
-            try {
-                fileReader!!.close()
-                csvReader!!.close()
-            } catch (e: IOException) {
-                println("Closing fileReader/csvParser Error!")
-                e.printStackTrace()
-            }
-        }
-
-    }
+//    private fun readCSV(application: Application) {
+//
+//        var fileReader: BufferedReader? = null
+//        var csvReader: CSVReader? = null
+//        try {
+//            fileReader = applicationContext.assets.open("Export_Ramal.csv").bufferedReader()
+//            csvReader = CSVReaderBuilder(fileReader).
+//                    withCSVParser(CSVParserBuilder().
+//                            withSeparator(';').
+//                            withIgnoreQuotations(false).
+//                            withFieldAsNull(CSVReaderNullFieldIndicator.BOTH).
+//                            build()).
+//                    withSkipLines(1).build()
+//            var line: Array<String>?
+//            line = csvReader.readNext()
+//            while (line != null) {
+//                var item = Item(
+//                        number = line[0],
+//                        name = line[1],
+//                        deploymentState = line[2],
+//                        incidentState = line[3],
+//                        vendor = line[4],
+//                        model = line[5],
+//                        description = line[6],
+//                        type = line[7],
+//                        owner = line[8],
+//                        serialNumber = line[9]
+//                )
+//                val dataSource = ItemDatabase.getInstance(application).itemDao()
+//                println(dataSource.getRowsCount())
+//                //println(item)
+//                dataSource.insert(item)
+//                //PopulateDbAsync(item, application)
+//                line = csvReader.readNext()
+//            }
+//            csvReader.close()
+//        } catch (e: Exception) {
+//            println("Reading CSV Error!")
+//            e.printStackTrace()
+//        } finally {
+//            try {
+//                fileReader!!.close()
+//                csvReader!!.close()
+//            } catch (e: IOException) {
+//                println("Closing fileReader/csvParser Error!")
+//                e.printStackTrace()
+//            }
+//        }
+//
+//    }
 
 
 
--- app/src/main/java/com/example/alexandre/datacollector/WelcomeFragment.kt
+++ app/src/main/java/com/example/alexandre/datacollector/WelcomeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.alexandre.datacollector
 
 
+import android.app.Application
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -10,15 +11,29 @@ import android.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import com.example.alexandre.datacollector.databinding.WelcomeBinding
+import com.example.alexandre.datacollector.db.Item
+import com.example.alexandre.datacollector.db.ItemDatabase
+import com.opencsv.CSVParserBuilder
+import com.opencsv.CSVReader
+import com.opencsv.CSVReaderBuilder
+import com.opencsv.enums.CSVReaderNullFieldIndicator
+import kotlinx.coroutines.*
+import java.io.BufferedReader
+import java.io.IOException
+import kotlin.coroutines.CoroutineContext
 
 
 /**
  * A simple [Fragment] subclass.
  *
  */
-class WelcomeFragment : Fragment() {
+class WelcomeFragment : Fragment(), CoroutineScope {
 
     private lateinit var binding: WelcomeBinding
+
+    private lateinit var job: Job
+    override val coroutineContext: CoroutineContext
+        get() = job + Dispatchers.Main
 
     //Inflating and Returning the View with DataBindingUtil
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -27,7 +42,72 @@ class WelcomeFragment : Fragment() {
         binding.addMainButton.setOnClickListener { view: View ->
             view.findNavController().navigate(R.id.action_welcomeFragment_to_newItemFragment)
         }
+
+        val application = requireNotNull(this.activity).application
+        job = Job()
+        launch {
+            async(Dispatchers.Default) {
+                readCSV(application)
+            }
+        }
+
         return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
+    }
+
+    private fun readCSV(application: Application) {
+
+        var fileReader: BufferedReader? = null
+        var csvReader: CSVReader? = null
+        try {
+            fileReader = activity?.applicationContext?.assets?.open("Export_Hardware.csv")?.bufferedReader()
+            csvReader = CSVReaderBuilder(fileReader).
+                    withCSVParser(CSVParserBuilder().
+                            withSeparator(';').
+                            withIgnoreQuotations(false).
+                            withFieldAsNull(CSVReaderNullFieldIndicator.BOTH).
+                            build()).
+                    withSkipLines(1).build()
+            var line: Array<String>?
+            line = csvReader.readNext()
+            while (line != null) {
+                var item = Item(
+                        number = line[0],
+                        name = line[1],
+                        deploymentState = line[2],
+                        incidentState = line[3],
+                        vendor = line[4],
+                        model = line[5],
+                        description = line[6],
+                        type = line[7],
+                        owner = line[8],
+                        serialNumber = line[9]
+                )
+                val dataSource = ItemDatabase.getInstance(application).itemDao()
+                println(dataSource.getRowsCount())
+                //println(item)
+                dataSource.insert(item)
+                //PopulateDbAsync(item, application)
+                line = csvReader.readNext()
+            }
+            csvReader.close()
+        } catch (e: Exception) {
+            println("Reading CSV Error!")
+            e.printStackTrace()
+        } finally {
+            try {
+                fileReader!!.close()
+                csvReader!!.close()
+            } catch (e: IOException) {
+                println("Closing fileReader/csvParser Error!")
+                e.printStackTrace()
+            }
+        }
+
     }
 
 
